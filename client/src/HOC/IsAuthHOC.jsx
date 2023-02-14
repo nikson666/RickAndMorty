2064c7f86a30c70810f8b550ff8127f26ddf2174
Nikita <70951729+nikson666@users.noreply.github.com>
2023-02-14 23:07:57 +0200

    Update IsAuthHOC.jsx

--- client/src/HOC/IsAuthHOC.jsx
+++ client/src/HOC/IsAuthHOC.jsx
@@ -1,6 +1,28 @@
+import { Box } from "@mui/system";
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { getIsAuth } from "../redux/reducers/authReducer";
+
+const NotLoginPage = () => {
+  const style = {
+    wrapper: {
+      position: "absolute",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    }
+  };
+
+  return (
+    <Box style={style.wrapper}>
+      <h1>You are not authorized!</h1>
+    </Box>
+  )
+}
 
 const IsAuthHOC = (Component) => {
   const RedirectComponent = (props) => {
@@ -8,7 +30,7 @@ const IsAuthHOC = (Component) => {
       props.getIsAuth();
     }, []);
     
-    return props.isAuth ? <Component /> : <h1>you are not authorized!</h1>;
+    return props.isAuth ? <Component /> : <NotLoginPage />;
   };
 
   const mapStateToProps = (state) => ({
